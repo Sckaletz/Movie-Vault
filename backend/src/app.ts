82bad9835784dec77f6669c7952cb584886a7b08
Ellyas Zazi <142321449+donzazi@users.noreply.github.com>
2026-06-24 13:54:21 +0200

    Prometheus and Grafana

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,21 @@ import cors from "cors";
 import movieRoutes from "./routes/movies";
 import healthRoutes from "./routes/health";
 import seenRoutes from "./routes/seen";
+import { collectDefaultMetrics, register } from 'prom-client';
+
+// Start collecting default metrics (CPU, Memory, etc) immediately
+collectDefaultMetrics();
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 // Routes
 app.use("/api", healthRoutes);
